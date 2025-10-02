5f6f33b79fedf66cd7fa57c19c1ed15647d14188
Kaspar Bergert <kasparbergert6@gmail.com>
2025-10-03 00:47:51 +0300

    feat: created user table type checking

---
+++ Backend/models/userSchema.js
@@ -0,0 +1,33 @@
+import Joi from 'joi'
+
+const userSchema = Joi.object({
+  id: Joi.number().messages({
+    'number.base': 'Id is not a number or could not be cast to a number',
+  }),
+  username: Joi.string()
+    .pattern(/^[A-Za-z0-9_-]+$/)
+    .max(128)
+    .required()
+    .messages({
+      'string.pattern.base': 'username contains invalid characters.',
+      'string.max': 'username is too long',
+      'any.required': 'username required',
+    }),
+  password: Joi.string()
+    .pattern(/^[\x21-\x7E]$/)
+    .min(15)
+    .max(99)
+    .required()
+    .messages({
+      'string.pattern.base': 'password contains invalid characters',
+      'string.min': 'password must be at least 12 characters long',
+      'string.max': 'password is too long. must be <= 99',
+      'any.required': 'password required',
+    }),
+  email: Joi.string().email().required().messages({
+    'any.required': 'email required',
+    'string.email': 'Email must be a valida address',
+  }),
+})
+
+export default userSchema
