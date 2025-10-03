9a30675ad08269d37c8c54b0d062688294c9baea
Kaspar Bergert <kasparbergert6@gmail.com>
2025-10-04 02:00:06 +0300

    feat: console.error text now displays red deps: added cookie-parser

--- Backend/api/src/server.js
+++ Backend/api/src/server.js
@@ -1,12 +1,26 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import '../../env.js'
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import CreateRoutes from './routes.js'
+import addMessageField from './middleware/addMessageField.js'
+import chalk from 'chalk'
+
+//colors the error to red
+const origError = console.error
+console.error = (error) => {
+  origError(chalk.red(error))
+}
 
 const app = express()
 const PORT = process.env.PORT
 const URI = process.env.URI
 
+const middlewares = [addMessageField]
+
+app.use(cookieParser())
+app.use(express.json())
+
+app.use(middlewares)
 app.use('/api', CreateRoutes())
 
 app
