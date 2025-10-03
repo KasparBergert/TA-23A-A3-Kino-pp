f0e6a55399c944fb5eeabea899a3b645a63357d4
Kaspar Bergert <kasparbergert6@gmail.com>
2025-10-04 01:57:47 +0300

    fix: login and register had the same route

--- Backend/api/src/routes.js
+++ Backend/api/src/routes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express'
-import login from './auth/users/login'
-import register from './auth/users/register'
-import access from './auth/JWT/access'
+import login from './auth/users/login.js'
+import register from './auth/users/register.js'
+import access from './auth/JWT/refresh.js'
 
 export default function CreateRoutes() {
   const router = Router()
-  router.post('/auth/users', login)
-  router.post('/auth/users', register)
+  router.post('/auth/users/login', login)
+  router.post('/auth/users/register', register)
   router.get('/auth/JWT', access)
   return router
 }
