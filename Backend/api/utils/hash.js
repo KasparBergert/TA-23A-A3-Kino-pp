861c2e7b3590b4fae0f6c15933cd76f4899adb50
Kaspar Bergert <kasparbergert6@gmail.com>
2025-10-04 01:56:56 +0300

    feat: added helper function to hash passwords

---
+++ Backend/api/utils/hash.js
@@ -0,0 +1,23 @@
+import argon2 from 'argon2'
+/**
+ * @param {string} plain - the password to hash
+ * @returns {Buffer} argon parameters + 64 byte hash
+ */
+export default async function hashPassword(plain) {
+  return await argon2.hash(plain, {
+    type: 2,
+    parallelism: 6,
+    hashLength: 64,
+    memoryCost: 2 ** 16 - 1, //64 MB
+    //rest are set to default
+  })
+}
+
+/**
+ * @param {string} hash - Argon2id hash
+ * @param {*} plain - password to check
+ * @returns {Boolean} true or false if the password matches
+ */
+export function verifyPassword(hash, plain) {
+  return argon2.verify(hash, plain)
+}
