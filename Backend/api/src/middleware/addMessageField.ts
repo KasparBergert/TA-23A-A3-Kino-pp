229503672ba04fca0fb3755fbcdcdb1abaceeafb
Kaspar Bergert <50704267035@tptlive.ee>
2025-10-06 13:35:13 +0300

    fix: Edited error messages to be more descriptive

--- Backend/api/src/middleware/addMessageField.ts
+++ Backend/api/src/middleware/addMessageField.ts
@@ -6,8 +6,8 @@ export default function addMessageField(req: Request, res: Response, next: NextF
 
   res.send = function (body) {
     if (typeof body == 'object' && !('code' in body)) {
-      // throwing an error because sending a response without a code is against API standards.
-      throw Error("Response body doesn't include a 'code' field or is not an 'object'")
+      // throwing an error because sending a response without a code is against API standards.      
+      throw Error("HEY developer. you have to add a 'code' field for a message field to appear")      
     }
 
     if (typeof body == 'object' && 'code' in body && !('message' in body)) {
@@ -15,8 +15,8 @@ export default function addMessageField(req: Request, res: Response, next: NextF
       const message = codeToMessage(code)
       if (!message) {
         // message is undefined
-        // throwing an error because every code should have its own message.
-        throw Error("code doesn't have a corresponding messsage")
+        // throwing an error because every code should have its own message.        
+        throw Error("HEY developer. you have to add a message that matches the code.")
       }
       body = { ...body, message }
     }
