6d27202c1a74e09fd4d9210df1d919af6a9cc316
Kaspar Bergert <kasparbergert6@gmail.com>
2025-10-09 22:27:10 +0300

    wip: added Showtime.vue

--- Frontend/src/router/index.ts
+++ Frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home/HomeView.vue'
+import Showtime from '../views/Showtimes/Showtime.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/showtimes',
+      name: 'showtimes',
+      component: Showtime,
     },
   ],
 })
