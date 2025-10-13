155b0e0ffeaa4e6842511f396a24afb2302eb2e8
Kaspar Bergert <kasparbergert6@gmail.com>
2025-10-13 23:15:24 +0300

    deps: removed vuetify and added flowbite with tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true })],
+  plugins: [vue(), tailwindcss()],
 })
