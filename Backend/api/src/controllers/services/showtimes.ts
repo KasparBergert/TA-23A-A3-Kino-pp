ba47fb388f9a9382d8573e1475dbb1ee76648650
miiksnii <kaspar.bergert@tptlive.ee>
2025-10-29 14:00:07 +0200

    wip: showtimes fetching in api

--- Backend/api/src/controllers/services/showtimes.ts
+++ Backend/api/src/controllers/services/showtimes.ts
@@ -1,6 +1,7 @@
 import { Response } from 'express'
 import prisma from '../../../../database/db'
 import getFilmTitle from '../../../services/getfilmTitle'
+import { toUSVString } from 'util'
 
 //gets all available theatres
 export default async function showtimes(req: Request, res: Response) {
@@ -12,8 +13,53 @@ export default async function showtimes(req: Request, res: Response) {
       it will only return everything about the showtime.
     */
 
+    /*
 
-    getall films
+    SELECT
+    t.`name` AS theatre_name,
+    f.title AS film_name,
+    f.duration_min,
+    f.poster_url,
+    h.name AS hall_name,
+    (h.capacity - count(s.id)) AS available_seats,
+    price,
+    starts_at,
+    ends_at
+    FROM
+    showtimes st
+    JOIN films f ON st.film_id = f.id
+    JOIN halls h ON st.hall_id = h.id
+    JOIN theatre_halls th ON th.hall_id = h.id
+    JOIN theatres t ON t.id = th.theatre_id
+    JOIN seats s ON s.hall_id = h.id
+    GROUP BY film_name;
+
+     */
+
+      const showtimesUnparsed = await prisma.showtimes.findMany({
+        select:{
+         price: true,
+         starts_at: true,
+         ends_at: true,
+          films: {
+            select: {
+              title: true,
+              duration_min: true,
+              poster_url: true
+            }
+          },
+          halls: {
+            select:{
+              name: true,
+              capacity: true
+            }
+          }
+        },
+
+      })
+
+
+
 
 
      {
