31902dbb985e3ae0874068e4dc3e463a153ca6e5
Kaspar Bergert <kasparbergert6@gmail.com>
2025-10-31 14:50:30 +0200

    feat: created showtimes api endpoint

--- Backend/api/src/controllers/services/showtimes.ts
+++ Backend/api/src/controllers/services/showtimes.ts
@@ -1,86 +1,74 @@
-import { Response } from 'express'
 import prisma from '../../../../database/db'
-import getFilmTitle from '../../../services/getfilmTitle'
-import { toUSVString } from 'util'
+import { Request, Response } from 'express'
 
 //gets all available theatres
 export default async function showtimes(req: Request, res: Response) {
 
-  //filters
-    /*
-    - theatre id
-      it should filter the showtimes based on the films in the theare
-      it will only return everything about the showtime.
-    */
+const showtimes = await prisma.showtimes.findMany({
+  select: {
+    id: true,
+    price: true,
+    starts_at: true,
+    ends_at: true,
+    film_id: true,
+    hall_id: true,
+  },
+});
 
-    /*
+const filmIds = showtimes.map(st => st.film_id);
+const films = await prisma.films.findMany({
+  where: { id: { in: filmIds } },
+  select: {
+    id: true,
+    title: true,
+    duration_min: true,
+    poster_url: true,
+  },
+});
 
-    SELECT
-    t.`name` AS theatre_name,
-    f.title AS film_name,
-    f.duration_min,
-    f.poster_url,
-    h.name AS hall_name,
-    (h.capacity - count(s.id)) AS available_seats,
-    price,
-    starts_at,
-    ends_at
-    FROM
-    showtimes st
-    JOIN films f ON st.film_id = f.id
-    JOIN halls h ON st.hall_id = h.id
-    JOIN theatre_halls th ON th.hall_id = h.id
-    JOIN theatres t ON t.id = th.theatre_id
-    JOIN seats s ON s.hall_id = h.id
-    GROUP BY film_name;
+const hallIds = showtimes.map(st => st.hall_id);
+const halls = await prisma.halls.findMany({
+  where: { id: { in: hallIds } },
+  select: {
+    id: true,
+    name: true,
+    theatre_id: true,
+  },
+});
 
-     */
+const seats = await prisma.seats.findMany({
+  where: { hall_id: { in: hallIds } },
+  select: {
+    id: true,
+    hall_id: true,
+    is_available: true,
+  },
+});
 
-      const showtimesUnparsed = await prisma.showtimes.findMany({
-        select:{
-         price: true,
-         starts_at: true,
-         ends_at: true,
-          films: {
-            select: {
-              title: true,
-              duration_min: true,
-              poster_url: true
-            }
-          },
-          halls: {
-            select:{
-              name: true,
-              capacity: true
-            }
-          }
-        },
+const theatreIds = halls.map(h => h.theatre_id);
+const theatres = await prisma.theatres.findMany({
+  where: { id: { in: theatreIds } },
+  select: { id: true, name: true },
+});
 
-      })
+const showtimesFull = showtimes.map(st => {
+  const film = films.find(f => f.id === st.film_id);
+  const hall = halls.find(h => h.id === st.hall_id);
+  const hallSeats = seats.filter(s => s.hall_id === hall?.id);
+  const theatre = theatres.find(t => t.id === hall?.theatre_id);
 
+  return {
+    price: st.price,
+    starts_at: st.starts_at,
+    ends_at: st.ends_at,
+    films: film,
+    halls: {
+      name: hall?.name,
+      available_seats: hallSeats.reduce((acc, seat) => (acc += seat.is_available), 0),
+    },
+    theatre_name: theatre ? theatre.name : null,
+  };
+});
 
-
-
-
-     {
-      film: {
-        title: "title",
-        release_date: "date",
-        poster_url: "URL",
-        genres: ['here', 'there']
-      },
-      theatre: {
-        id: 12,
-        name: "name"
-      },
-      hall: {
-        id: 12,
-        name: "name"
-        available_seats: 123
-      },
-      starts_at: (javascript date)
-      ends_at: (javascript date)
-    }
-
-  res.status(400).send({ code: 'VAL-0001' })
+  res.status(400).send({ code: 'VAL-0000', showtimesFull })
 }
