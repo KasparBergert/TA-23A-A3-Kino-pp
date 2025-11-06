27f8560a27e677880d689eda2c340052ef849ef3
Kaspar Bergert <kasparbergert6@gmail.com>
2025-11-06 23:45:48 +0200

    chore: changed navbar color on homepage

--- Frontend/src/components/TheNavbar.vue
+++ Frontend/src/components/TheNavbar.vue
@@ -20,8 +20,7 @@ import FormRegister from "../features/auth/FormRegister.vue";
 <style scoped>
 /* Layout */
 .navbar {
-  background: white;
-  border-bottom: 1px solid #e5e7eb;
+  background: rgba(8, 25, 179, 0.301);
   padding: 0.75rem 1rem;
   box-shadow: 0 1px 3px rgba(0,0,0,0.08);
   display: flex;
@@ -32,7 +31,7 @@ import FormRegister from "../features/auth/FormRegister.vue";
 .navbar-title {
   font-size: 1.25rem;
   font-weight: 600;
-  color: #1f2937;
+  color:white;
 }
 
 .navbar-right {
@@ -41,13 +40,13 @@ import FormRegister from "../features/auth/FormRegister.vue";
 }
 
 .nav-btn {
-  color: #374151;
+  color:white;
   background: none;
   border: none;
   font-weight: 500;
   cursor: pointer;
 }
 .nav-btn:hover {
-  color: #2563eb;
+  color: #269af8;
 }
 </style>
