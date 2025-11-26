ec97dc1a33484929ed6d570090a84cfc287dc8a0
Kaspar Bergert <kasparbergert6@gmail.com>
2025-11-26 20:18:18 +0200

    feat: implement theatre retrieval by ID and improve error handling

--- Backend/src/controllers/getTheatres.ts
+++ Backend/src/controllers/getTheatres.ts
@@ -1,13 +1,23 @@
-import theatreService from '../repositories/TheatreRepository'
+import theatreService from '../services/TheatreService';
 import { Request, Response } from 'express'
 
 //gets all available theatres
 export default async function getTheatres(req: Request, res: Response) {
   try {
+    const query = req.query
+    //check if request has theatre_id
+    const theatre_id = Number(query.theatre_id);
+    if (!Number.isNaN(theatre_id)) {
+      const theatre = await theatreService.getById(theatre_id)
+      return res.status(200).send({ theatre })
+    }else if(theatre_id){
+      return res.status(400).send(`Invalid theatre_id ${typeof theatre_id} instead of number`)
+    }
+
     const theatres = await theatreService.getAll()
     return res.status(200).send({ theatres })
   } catch (err) {
     console.error(err)
-    return res.status(400).send('Error occured fetching theatres')
+    return res.status(400).send(`Error occured fetching theatres. \n${err}`)
   }
 }
--- Backend/src/services/TheatreService.ts
+++ Backend/src/services/TheatreService.ts
@@ -8,6 +8,15 @@ class TheatreService {
   async getAll(): Promise<theatres[]> {
     return await theatreRepository.getAll()
   }
+
+  async getById(theatre_id: number): Promise<theatres> {
+    const res = await theatreRepository.getById(theatre_id);
+    if(res === null){
+      throw new Error("THEATRE_NOT_FOUND")
+    }
+    return res;
+  }
+
 }
 
 const theatreService = new TheatreService()
