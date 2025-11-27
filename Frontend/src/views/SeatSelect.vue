d4fc4be0e8093f65dfb344da372df995c23c42c5
Kaspar Bergert <kasparbergert6@gmail.com>
2025-11-28 00:49:00 +0200

    feat: implement seat selection routing and improve error handling for showtime navigation wip: seat select page in progress

---
+++ Frontend/src/views/SeatSelect.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const showtimeId = route.query.showtime_id as string;
+
+console.log(showtimeId)
+</script>
+<template>
+
+show a 2D grid of seats. fetch the seats from a hall.
+
+1. fetch the showtime hall seats
+2. display them in a grid
+2.1 disable selection of already booked seats
+3. user selectes seats
+4. user confirms selection and goes to payment page
+
+</template>
