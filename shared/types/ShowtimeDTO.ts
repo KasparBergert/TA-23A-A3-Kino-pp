c5d1af6a09a852568c1453f0233898e4e80d18d4
Kaspar Bergert <kasparbergert6@gmail.com>
2025-11-29 20:05:13 +0200

    feat: add FilmDTO

--- shared/types/ShowtimeDTO.ts
+++ shared/types/ShowtimeDTO.ts
@@ -1,30 +1,23 @@
+import FilmDTO from './FilmDTO'
 interface ShowtimeDTO {
   id: number
-  ends_at: Date | null;
-  is_canceled: boolean;
-  price: number;
-  starts_at: Date;
+  ends_at: Date | null
+  is_canceled: boolean
+  price: number
+  starts_at: Date
 
-  film: {
-    id: number;
-    title: string;
-    description: string | null;
-    duration_min: number | null;
-    poster_url: string | null;
-    release_date: Date | null;
-  };
+  film: FilmDTO
 
   hall: {
-    id: number;
-    name: string;
-    capacity: number;
-  };
+    id: number
+    name: string
+    capacity: number
+  }
 
   theatre: {
-    id: number;
-    name: string;
-  };
+    id: number
+    name: string
+  }
 }
-
 
 export default ShowtimeDTO
