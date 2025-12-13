65eb179e09a603de513352aba0164feeb5192b00
Kaspar Bergert <kasparbergert6@gmail.com>
2025-12-13 23:59:33 +0200

    fix(seed): seed sometimes chose admin as the user, triggering prevent_admin_buy_tickets, causing and error.

--- Backend/prisma/seed/data/OrdersData.ts
+++ Backend/prisma/seed/data/OrdersData.ts
@@ -1,21 +1,22 @@
-import { orders } from "@prisma/client"
+import { orders, orders_status, users_role } from "@prisma/client"
 import { getRandom } from "../utils/fetch"
 import userRepository from "../../../src/repositories/UserRepository"
 
 export async function createOrdersSeed(): Promise<Omit<orders, 'id' | 'created_at'>[]> {
-  const users = await userRepository.getAll()
+  const usersRaw = await userRepository.getAll()
+  const users = usersRaw.filter(user => user.role === users_role.user); //get rid of admin users
 
   if (users.length === 0) throw new Error("No users exist for orders")
 
   return [
     {
       user_id: getRandom(users).id,
-      status: 'pending',
+      status: orders_status.pending,
       expires_at: new Date('2025-11-01T19:00:00'),
     },
     {
       user_id: getRandom(users).id,
-      status: 'paid',
+      status: orders_status.paid,
       expires_at: null,
     },
   ]
