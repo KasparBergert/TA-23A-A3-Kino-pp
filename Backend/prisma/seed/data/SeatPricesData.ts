868d4070b2e975166408b5f0ffe5b30271dae823
Kaspar Bergert <kasparbergert6@gmail.com>
2026-02-06 12:51:54 +0200

    chore: hardcode seatPrices in seedData

--- Backend/prisma/seed/data/SeatPricesData.ts
+++ Backend/prisma/seed/data/SeatPricesData.ts
@@ -1,14 +1,21 @@
-import { seatType } from "@prisma/client"
+import { PrismaClient, seatType } from '@prisma/client'
 
-function seatPrices(){
-  const typeList: seatType[] = [seatType.Double, seatType.Premium, seatType.Standard]
-  return typeList.map((type) => {
-    return {
-      type: type,
-      price: Math.floor(Math.random() * 19)
-    }
-  })
+function seatPrices() {
+  return [
+    {
+      type: seatType.Double,
+      price: 14,
+    },
+    {
+      type: seatType.Premium,
+      price: 16,
+    },
+    {
+      type: seatType.Standard,
+      price: 9,
+    },
+  ]
 }
 
-const seatPricesSeed = seatPrices();
+const seatPricesSeed = seatPrices()
 export default seatPricesSeed
